9209ce69d14263c4b105eea58437c44f15c3afaa
Philipp Hecht <philipp@derhecht.de>
2020-12-01 23:25:34 +0100

    add coverage stuff

--- ram/run.py
+++ ram/run.py
@@ -1,11 +1,12 @@
 #!/bin/python
 from os.path import join, dirname
-#from subprocess import call
+from subprocess import call
 from vunit import VUnit
 
-#def post_run(results):
-#    results.merge_coverage(file_name="coverage_data")
-#    call(["gcovr", "coverage_data"])
+def post_run(results):
+    results.merge_coverage(file_name="coverage_data")
+    if VU.get_simulator_name() == "ghdl":
+        call(["gcovr", "coverage_data"])
 
 def create_test_suite(prj):
     root = dirname(__file__)
@@ -23,10 +24,13 @@ def create_test_suite(prj):
         lib.set_compile_option("ghdl.a_flags", ["--std=08", "--ieee=synopsys", "-frelaxed-rules"])
         lib.set_sim_option("ghdl.elab_flags", ["--ieee=synopsys", "-frelaxed-rules"])
 
+        lib.set_sim_option("enable_coverage", True)
+        lib.set_compile_option("enable_coverage", True)
+
     tb_ram_sp = lib.test_bench("tb_ram_sp")
 
-    addrw = [8,9,10]
-    widths = [8,9,16,18,31]
+    addrw = [8, 9, 10]
+    widths = [8, 16]
     oreg = [True, False]
     for test in tb_ram_sp.get_tests():
         for width in widths:
@@ -60,5 +64,5 @@ if __name__ == "__main__":
     VU = VUnit.from_argv()
     VU.add_osvvm()
     create_test_suite(VU)
-    VU.main()
-    #VU.main(post_run=post_run)
+    #VU.main()
+    VU.main(post_run=post_run)
